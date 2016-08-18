f58bb9c71531049c6bb17a8c2ff546e930f62cb1
Ilja Weis <Ilja.Weis@nexinto.com>
2016-08-18 15:18:10 +0200

    Do not fail when we have no access to mounts

--- vault-bash-completion.sh
+++ vault-bash-completion.sh
@@ -1,7 +1,15 @@
 function _vault() {
 
   local VAULT_COMMANDS='delete path-help read renew revoke server status write audit-disable audit-enable audit-list auth auth-disable auth-enable capabilities generate-root init key-status list mount mount-tune mounts policies policy-delete policy-write rekey remount rotate seal ssh step-down token-create token-lookup token-renew token-revoke unmount unseal version'
-  local VAULT_ROOTPATH=$(vault mounts | tail -n +2 | awk '{print $1}' | paste -s -d ' ' -)
+
+  # get root paths
+  vault mounts >/dev/null 2>&1
+  if [ $? != 0 ]; then
+    # we do not have access to list mounts
+    local VAULT_ROOTPATH="secret"
+  else
+    local VAULT_ROOTPATH=$(vault mounts | tail -n +2 | awk '{print $1}' | paste -s -d ' ' -)
+  fi
 
   local cur=${COMP_WORDS[COMP_CWORD]}
   local line=${COMP_LINE}
